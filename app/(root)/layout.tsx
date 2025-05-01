f65489c04ce239b54f471e4a6e01e463f7ca35d5
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-05-01 22:42:39 +0700

    first-commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const layout = ({children}: Readonly<{children: React.ReactNode}>) => {
+  return (
+    <main>
+        <Navbar/>
+        {children}
+        
+        </main>
+  )
+}
+
+export default layout
