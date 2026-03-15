59b440d62ce1f921a5deaf755df123505e3937f5
Rafif Ramadhana <rafiframadhana.cs@gmail.com>
2026-03-15 16:28:25 +0700

    fix build errors

--- auth.ts
+++ auth.ts
@@ -9,8 +9,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async signIn({
       user: { name, email, image },
-      profile: { id, login, bio },
+      profile,
     }) {
+      const { id, login, bio } = profile as { id: string; login: string; bio?: string };
       const existingUser = await client
         .withConfig({ useCdn: false })
         .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
